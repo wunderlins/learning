c8d12939cb15a7066ca4979efd7628a264520d08
wus <swunderlin@gmail.com>
2014-05-27 20:25:15 +0200

    function pointer in struct

--- C/src/function_pointer/fps.c
+++ C/src/function_pointer/fps.c
@@ -1,21 +1,36 @@
-#include <stdio.h>
+//#define _USE_MATH_DEFINES
+//#define _BSD_SOURCE
 
-void void_int(int x);
-void void_int(int x) {
-	printf( "void_int: %d\n", x);
+#include <stdio.h>
+#include <math.h>
+
+#ifndef M_PI
+#	define M_PI 3.14159265358979323846
+#endif
+
+typedef struct sphere {
+	int diameter;
+	void (*volume)(int);
+} sphere;
+
+void volume(int);
+void volume(int r) {
+	double volume = (4/3) * M_PI * pow(r, 3);
+	printf( "volume: %f\n", volume);
 }
 
 int main(int argc, char **argv) {
-	printf("Hello world\n");
 	
-	// declare a function pointer
-	void (*by_ref_void_int)(int);
+	// create a sphere
+	sphere s;
+	s.diameter = 3;
 	
 	// assign pointer to a function
-	by_ref_void_int = &void_int;
+	s.volume = &volume;
 	
 	// run referenced function
-	by_ref_void_int(2);
+	s.volume(s.diameter);
 	
 	return 0;
 }
+
