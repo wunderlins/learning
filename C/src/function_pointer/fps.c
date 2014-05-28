2392b52a58343575614732849e9d6be6d5d05cd9
wus <swunderlin@gmail.com>
2014-05-28 20:21:40 +0200

    commented

--- C/src/function_pointer/fps.c
+++ C/src/function_pointer/fps.c
@@ -1,3 +1,9 @@
+/**
+ * Function Pointer Example
+ *
+ * (c) 2014, Simon Wudnerlin, <swunderlin()gmailDTcom>
+ */
+
 //#define _USE_MATH_DEFINES
 //#define _BSD_SOURCE
 
@@ -17,10 +23,20 @@ double volume(int);
 sphere_t get_sphere(int);
 double sphere_volume(sphere_t*);
 
+/**
+ * Calculate volume for a sphere
+ *
+ * Expects parameter 1 to be the diameter.
+ */
 double volume(int diameter) {
 	return (double) ((4/3) * M_PI * pow((diameter/2), 3));
 }
 
+/**
+ * Constructor for sphere_t
+ *
+ * Allocates memory and initializes a sphere_t object.
+ */
 sphere_t get_sphere(int r) {
 	sphere_t s;
 	s.diameter = r;
@@ -29,15 +45,25 @@ sphere_t get_sphere(int r) {
 	return s;
 }
 
+/**
+ * Compute a sphere's volume
+ *
+ * The sphere_t provides a method sphere_t.volume(). Use the diameter and
+ * this method to calculate the volume of a sphere.
+ */
 double sphere_volume(sphere_t *s) {
 	return s->volume(s->diameter);
 }
 
+/**
+ * Main method
+ */
 int main(int argc, char **argv) {
 	
-	// create a sphere
+	// create a sphere object
 	sphere_t s = get_sphere(3);
 	
+	// calculate the spere's volume
 	double res = sphere_volume(&s);
 	printf("%f\n", res);
 	
