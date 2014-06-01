1d665618e916da417137681c1d4f97f23788b509
wus <swunderlin@gmail.com>
2014-06-01 21:19:50 +0200

    added skeleton

---
+++ C/src/bitfields/bitfields.c
@@ -0,0 +1,14 @@
+/**
+ * Bit-Fields Example
+ *
+ * (c) 2014, Simon Wunderlin, <swudnerlin()gmailDTcom>
+ */
+
+#include <stdio.h>
+
+/**
+ * Main method
+ */
+int main(int argc, char **argv) {
+	return 0;
+}
