9bd4ede36d615d5347beb70a63c98683b87247a8
wus <swunderlin@gmail.com>
2014-06-17 00:20:38 +0200

    added signal_map

--- C/src/signal/send.c
+++ C/src/signal/send.c
@@ -1,12 +1,19 @@
 // #define _GNU_SOURCE // required by strsignal on linux
 
 #include <stdio.h>
-// #include <string.h>
 #include <stdlib.h>
- #include <signal.h>
+#include <signal.h>
+// #include <string.h>
 // #include <unistd.h>
+#include <sys/types.h>
+#include <unistd.h>
+//#include <fcntl.h>
 
 int main(int argc, char *argv[]) {
+
+	int signal_map[1024];
+	for(int n=1; n < NSIG; ++n)
+		signal_map[strsignal(n)] = n;
 
 	// TODO: implement signal sending
 	printf("Parameters: <SIGNAL>%s <PID>%d\n", argv[1], atoi(argv[2]));
