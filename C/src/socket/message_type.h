4b15b77ff06575fc1b05349114b3e0fb8fc44def
wus <swunderlin@gmail.com>
2014-06-16 19:33:08 +0200

    reformatted

--- C/src/socket/message_type.h
+++ C/src/socket/message_type.h
@@ -13,7 +13,16 @@
 #include <stdint.h>
 
 const char end_message = '\0';
-enum message_type_t {request=1, response=2, error=4, arg4=8, arg5=16, arg6=32, arg7=64, arg8=128};
+enum message_type_t {
+	request=1, 
+	response=2, 
+	error=4, 
+	arg4=8, 
+	arg5=16,
+	arg6=32, 
+	arg7=64, 
+	arg8=128
+};
 typedef struct {
 	enum message_type_t type;
 	uint8_t length;
