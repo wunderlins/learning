b8ad1863432244fec8696eabfe9360575716d95e
Simon Wunderlin <simon.wunderlin@usb.ch>
2014-06-17 15:23:14 +0200

    implemented converting char signal name to int

--- C/src/signal/send.c
+++ C/src/signal/send.c
@@ -1,22 +1,43 @@
 // #define _GNU_SOURCE // required by strsignal on linux
 
+#include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <signal.h>
-// #include <string.h>
-// #include <unistd.h>
+#include <string.h>
+#include <strings.h>
 #include <sys/types.h>
 #include <unistd.h>
 //#include <fcntl.h>
 
+#include "sig.h"
+#define ARRAY_SIZE(arr) (sizeof(arr) / sizeof(arr[0]))
+
+int signame_to_signum(char *sig) {
+	size_t n;
+	for (n = 0; n < ARRAY_SIZE(sys_signame); n++) {
+		if (!strcasecmp(sys_signame[n].name, sig))
+			return sys_signame[n].val;
+	}
+	return -1;
+}
+
 int main(int argc, char *argv[]) {
 
+	/*
 	int signal_map[1024];
 	for(int n=1; n < NSIG; ++n)
 		signal_map[strsignal(n)] = n;
+	*/
 
 	// TODO: implement signal sending
-	printf("Parameters: <SIGNAL>%s <PID>%d\n", argv[1], atoi(argv[2]));
+	char subsig[10];
+	memcpy(subsig, &(argv[1][3]), strlen(argv[1])-3);
+	subsig[strlen(argv[1])-3] = '\0';
+	int sign = signame_to_signum(subsig);
+	int pid = atoi(argv[2]);
+	//printf("sig: %d, %d, %s\n", sign, (int)strlen(argv[1]), subsig);
+	printf("Parameters: <SIGNAL>%d <PID>%d\n", sign, pid);
 
 	return 0;
 }
