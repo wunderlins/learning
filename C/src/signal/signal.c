3d100fb723caf591b39269e4b9f168a6b8640a35
Simon Wunderlin <simon.wunderlin@usb.ch>
2014-06-18 10:34:35 +0200

    added process forkin, the signal process is now daemonized

--- C/src/signal/signal.c
+++ C/src/signal/signal.c
@@ -106,6 +106,8 @@
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <errno.h>
+#include <sys/ioctl.h>
+#include <fcntl.h>
 
 #include "sig.h"
 
@@ -221,9 +223,39 @@ int main(int argc, char *argv[]) {
 	signal(SIGTTOU, sig_handler); //   22,22,27    Stop    tty output for background process
 	*/
 
-	printf("Process id: %d\n", getpid());
+	printf("Detatching from terminal\n");
+	printf("Capturing signals ...\n");
+	
+	pid_t pid = fork();
+	if (pid == -1)
+		fprintf(stderr, "Failed to fork\n");
+	else if (pid != 0) 
+		exit(0);
 
-	// successfully started, write pid file
+	//int r = ioctl(0, TIOCNOTTY, NULL);
+	//if (r != 0)
+	//	fprintf(stderr, "Failed to detatch\n");
+
+	// Start a new session for the daemon.
+	if (setsid()==-1) {
+		fprintf(stderr, "failed to become a session leader while "
+		                "daemonising(errno=%d)",errno);
+		exit(3);
+	}
+	
+	signal(SIGHUP,SIG_IGN);
+
+	// Fork again, allowing the parent process to terminate.
+	signal(SIGHUP,SIG_IGN);
+	pid = fork();
+	if (pid == -1) {
+		fprintf(stderr, "failed to fork while daemonising (errno=%d)",errno);
+    exit(2);
+	} else if (pid != 0) {
+		exit(0);
+	}
+	//printf("Process id: %d\n", getpid());
+  // successfully started, write pid file
 	fpp = fopen(signals_pid, "w");
 	if(fpp == NULL) {
 		printf("Error %d, %s\n", errno, strerror(errno));
@@ -232,8 +264,33 @@ int main(int argc, char *argv[]) {
 	}
 	fprintf(fpp, "%d", getpid());
 	fclose(fpp);
-
-	printf("Capturing signals ...\n");
+	printf("Process id: %d\n", getpid());
+	
+	if (chdir("/") == -1) {
+		fprintf(stderr, "failed to change working directory "
+	                  "while daemonising (errno=%d)", errno);
+		exit(2);
+	}
+	
+	// Set the user file creation mask to zero.
+	umask(0);
+    
+	// Close then reopen stdin, stdout and stderr	
+	close(STDIN_FILENO);
+	close(STDOUT_FILENO);
+	close(STDERR_FILENO);
+	if (open("/dev/null",O_RDONLY) == -1) {
+		fprintf(stderr,"failed to reopen stdin while daemonising (errno=%d)", errno);
+		exit(3);
+	}
+	if (open("/dev/null",O_WRONLY) == -1) {
+		fprintf(stderr, "failed to reopen stdout while daemonising (errno=%d)", errno);
+		exit(3);
+	}
+	if (open("/dev/null",O_RDWR) == -1) {
+		fprintf(stderr, "failed to reopen stderr while daemonising (errno=%d)", errno);
+		exit(3);
+	}
 
 	useconds_t usec = 1000000L;
 	while(1) {
