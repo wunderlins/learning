a2b007a6e2f08e3a6cb7556c5ed3c34026d0e1ae
wus <swunderlin@gmail.com>
2014-06-22 21:44:17 +0200

    added skeleton for multiplexed i/o

---
+++ C/system programming/2_io/multiplex/multiplex.c
@@ -0,0 +1,19 @@
+/**
+ * Multiplexed I/O allows an application to concurrently block on multiple file
+ * descriptors and receive notification when any one of them becomes ready to
+ * read or write without blocking. Multiplexed I/O thus becomes the pivot
+ * point for the application, designed similarly to the following activity:
+ *
+ * 1. Multiplexed I/O: Tell me when any of these file descriptors become
+ *    ready for I/O.
+ * 2. Nothing ready? Sleep until one or more file descriptors are ready.
+ * 3. Woken up! What is ready?
+ * 4. Handle all file descriptors ready for I/O, without blocking.
+ * 5. Go back to step 1.
+ */
+
+
+int main(int argc, char *argv[]) {
+
+	return 0;
+}
