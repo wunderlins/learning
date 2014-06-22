42ea0f29e4f2db896666265fcfe123cde17b0244
wus <swunderlin@gmail.com>
2014-06-22 13:02:30 +0200

    added safe read example.

--- C/system programming/2_io/read.c
+++ C/system programming/2_io/read.c
@@ -1,3 +1,4 @@
+#include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
 #include <stdio.h>
@@ -27,10 +28,56 @@ int main(int argc, char *argv[]) {
 	fd = open("/dev/null", O_RDONLY);
 	if (fd == -1) {
 		printf("failed to open, %d %s\n", errno, strerror(errno));	
-	} else {
-		printf("Successfully opened /dev/null");
-		close(fd);
+		exit(1);
 	}
+
+	printf("Successfully opened /dev/random\n");
+
+	// let's read some bogus length from the file
+	unsigned long word;
+	ssize_t nr;
+	/* read a couple bytes into 'word' from 'fd' */
+	nr = read (fd, &word, sizeof(unsigned long));
+	if (nr == -1) {
+		perror("read");
+	}
+
+	// the above example did not read as many characters as we wanted
+	printf("Attempted to read %ld bytes, got %d bytes\n", sizeof(unsigned long), (int) nr);
+	close(fd);
+
+	// opening a test file, it's input should be shorter than the attempted
+	// length to read
+	fd = open("test_input.txt", O_RDONLY);
+	if (fd == -1) {
+		printf("failed to open, %d %s\n", errno, strerror(errno));
+		exit(1);
+	}
+
+	// doing it the proper way, handling premature end of input
+	#define BUFSIZE 1024
+	char buf[BUFSIZE];
+	int len = 20;
+	ssize_t ret;
+	int rd = 0;
+	int pass = 0;
+	while (len != 0 && (ret = read (fd, buf, len)) != 0) {
+		if (ret == -1) {
+			if (errno == EINTR)
+				continue;
+			perror ("read");
+			break;
+		}
+		len -= ret;
+		rd += (int) ret;
+		pass++;
+		//buf += (int) ret;
+	}
+	printf("Attempted to read %d bytes in %d passes, got %d bytes in buffer\n",
+			   (int) rd, pass, (int) strlen(buf));
+	printf("Buffer: '%s'\n", buf);
+
+	close(fd);
 
 	return 0;
 }
