600e39e2aec7760f862dae6859e3574e5f165338
wus <swunderlin@gmail.com>
2014-06-24 19:29:49 +0200

    basig file read implementation

--- java/applet/upload/upload.java
+++ java/applet/upload/upload.java
@@ -1,9 +1,86 @@
+//public class UploadApplet { //.java
+import java.io.*;
+import java.awt.*; 
+import java.awt.event.*;
 import java.applet.*;
+import javax.swing.*;
+import javax.swing.border.*;
 
-import java.awt.*;
+public class upload extends JApplet
+implements ActionListener {
+	private JPanel pane = null;
+	private JScrollPane scrolling = null;
+	private JTextPane fileBox = null;
+	private JTextField tfFilename = null;
+	private JButton butLoad = null;
+	private final String LOAD = "load";
 
-public class upload extends Applet {
-    public void paint( Graphics g ) {
-        g.drawString("Hello World 2",50,25);
+	public void init() {
+		try {
+			jbInit();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+    private String getStrParam( String name, String defaultVal ) {
+		String param = this.getParameter(name);
+		if (param != null)
+			return param;
+		return defaultVal;
     }
+	
+	// method which will read data from file, and return it in
+	// String
+	public String readFile(String fn) {
+		String thisLine, ret = "";
+		try {
+			FileInputStream fin =  new FileInputStream(fn);
+			BufferedReader myInput = new BufferedReader
+					(new InputStreamReader(fin));
+			while ((thisLine = myInput.readLine()) != null) {  
+				ret += thisLine + "\n";
+			}
+		} catch (Exception e) {
+			ret = "Cannot load, exception!";
+		}
+		return ret;
+	}
+
+	private void jbInit() throws Exception {
+		pane = new JPanel();
+		pane.setBounds(new Rectangle(0, 0, 500, 325));
+		pane.setLayout(null);
+		pane.setBorder(BorderFactory.createEtchedBorder(
+				EtchedBorder.LOWERED));
+		pane.setBackground(new Color(221, 194, 219));
+
+		fileBox = new JTextPane();
+		fileBox.setText("");
+		fileBox.setEditable(false);
+		scrolling = new JScrollPane(fileBox);
+		scrolling.setBounds(new Rectangle(16, 65, 295, 225));
+
+		tfFilename = new JTextField();
+		tfFilename.setText("");
+		tfFilename.setBounds(new Rectangle(16, 23, 206, 29));
+
+		butLoad = new JButton();
+		butLoad.setBounds(new Rectangle(231, 23, 80, 30));
+		butLoad.setText("Load");
+		butLoad.setActionCommand(LOAD);
+		butLoad.addActionListener(this);
+
+		pane.add(scrolling);
+		pane.add(tfFilename);
+		pane.add(butLoad);
+
+		setContentPane(pane);
+	}
+
+	public void actionPerformed(ActionEvent e) {
+		if (e.getActionCommand().equals(LOAD)) {
+			fileBox.setText(readFile(tfFilename.getText()));
+		}
+	}
 }
