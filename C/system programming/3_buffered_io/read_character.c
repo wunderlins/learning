6974c256ef1bd9f043a41fa4ca83153a68fae76d
Simon Wunderlin <simon.wunderlin@usb.ch>
2014-07-03 12:03:15 +0200

    read example

---
+++ C/system programming/3_buffered_io/read_character.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+	int c;
+	FILE *stream;
+	c = fgetc (stream);
+	
+	if (c == EOF)
+		/* error */
+	else
+		printf ("c=%c\n", (char) c);
+
+	return 0;
+}
