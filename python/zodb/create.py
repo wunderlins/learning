8aa7024df70fd981971a68e152554388f7dbaca0
wus <swunderlin@gmail.com>
2016-01-28 19:33:26 +0100

    added config files

--- python/zodb/create.py
+++ python/zodb/create.py
@@ -7,14 +7,22 @@
 # open the zope database
 import ZODB, ZODB.FileStorage
 
+"""
 #storage = ZODB.FileStorage.FileStorage('mydata.fs')
 #db = ZODB.DB(storage)
 
 # Storage shortcuts
 db = ZODB.DB('data/mydata.fs') # file database
 #db = ZODB.DB(None) # memmory database
-
 connection = db.open()
+"""
+
+# using a config file to open the database
+import ZODB.config
+db = ZODB.config.databaseFromURL('config.xml')
+connection = db.open()
+
+# get the root node
 root = connection.root
 
 # create a btree list of accounts
@@ -22,7 +30,7 @@ import BTrees.OOBTree
 from account import Account
 
 root.accounts = BTrees.OOBTree.BTree()
-root.accounts['account-3'] = Account()
+root.accounts['account-4'] = Account()
 
 # make changes persistent
 import transaction
