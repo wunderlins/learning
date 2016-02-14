374350076fcb64b47f12acac5ab0bf6858101a6d
wus <swunderlin@gmail.com>
2016-02-14 12:52:04 +0100

    changed license

--- js/angular1/minimal-example/client/simpleapp.js
+++ js/angular1/minimal-example/client/simpleapp.js
@@ -1,7 +1,7 @@
 /* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * License: GPL2
+ * Author: Simon Wunderlin
+ * Copyright, 2015, simon Wunderlin
  */
 var SimpleApp = angular.module("SimpleApp", ['ui.bootstrap']);
 
