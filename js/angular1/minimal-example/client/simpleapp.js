85fdb8c103b2afe8a79cbea70ca05782e13e4b47
wus <swunderlin@gmail.com>
2016-02-14 14:02:22 +0100

    added addItem method

--- js/angular1/minimal-example/client/simpleapp.js
+++ js/angular1/minimal-example/client/simpleapp.js
@@ -3,16 +3,30 @@
  * Author: Simon Wunderlin
  * Copyright, 2015, simon Wunderlin
  */
-var SimpleApp = angular.module("SimpleApp", ['ui.bootstrap']);
-
-function appController($scope) {
-  $scope.items = [
-    {name: "name 1", location: "location 1"},
-    {name: "name 3", location: "location 3"},
-    {name: "name 2", location: "location 2"}
-  ];
-};
-
-SimpleApp.controller("appController", appController);
-
-
+var SimpleApp = angular.module("SimpleApp", ['ui.bootstrap'])
+.factory("appFactory", function() {
+	var factory = {};
+	var items = [
+		{name: "name 1", location: "location 1"},
+		{name: "name 3", location: "location 3"},
+		{name: "name 2", location: "location 2"}
+	];
+	
+	factory.getItems = function() {
+		return items;
+	};
+	
+	return factory;
+})
+.controller("appController", function ($scope, appFactory) {
+	$scope.items = appFactory.getItems();
+	
+	$scope.addItem = function() {
+		$scope.items.push({
+			name: $scope.newItem.name,
+			location: $scope.newItem.location
+		});
+		$scope.newItem.name = "";
+		$scope.newItem.location = "";
+	};
+});
